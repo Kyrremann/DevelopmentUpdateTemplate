fcc9144e5d9b102c98d9e95b974c037a489bec77
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2015-09-23 13:48:55 +0200

    Major cleanup, and extracted a lot of the text

--- routes/main.rb
+++ routes/main.rb
@@ -3,9 +3,9 @@ class DUT < Sinatra::Application
     site_mode = Configuration.find_by_name("site_mode")
     if site_mode.value == "track"
       @tracks = Track.order("title DESC")
-      haml :track_index
+      haml :index_track
     else
-      haml :cfp_index
+      haml :index_cfp
     end
   end
 end
